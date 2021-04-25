0a311968dccacdcddf6dac4351cb93a056464ef7
Andre Wibbeke <tech@andre-wibbeke.de>
2021-04-25 14:21:27 +0200

    14_URLify: add solution

--- 14_URLify/index.js
+++ 14_URLify/index.js
@@ -1,1 +1,18 @@
+const replacementPatterns = [
+  [/ /g, "-"],
+  [/[.!'?#]/g, ""],
+];
 
+function replacer(string, patternArray) {
+  let str = string;
+  for (const pattern of patternArray) {
+    str = str.replace(pattern[0], pattern[1]);
+  }
+  return str;
+}
+
+function urlify(blogPostTitle) {
+  return replacer(blogPostTitle.toLowerCase().trim(), replacementPatterns);
+}
+
+export default urlify;
--- 14_URLify/test/test.js
+++ 14_URLify/test/test.js
@@ -1,1 +1,27 @@
 import { assert } from "chai";
+import urlify from "../index.js";
+
+describe("urlify function", () => {
+  it("should return a url friendly string", () => {
+    assert.strictEqual(urlify("My blog name!"), "my-blog-name");
+  });
+
+  it("should return a url friendly string", () => {
+    assert.strictEqual(urlify("Andre's blog."), "andres-blog");
+  });
+
+  it("should return a url friendly string", () => {
+    assert.strictEqual(urlify("Did you know?"), "did-you-know");
+  });
+
+  it("should return a url friendly string", () => {
+    assert.strictEqual(
+      urlify("#5 most recent updates"),
+      "5-most-recent-updates"
+    );
+  });
+
+  it("should return a url friendly string", () => {
+    assert.strictEqual(urlify(" tHiS iS !aweSomE! "), "this-is-awesome");
+  });
+});
